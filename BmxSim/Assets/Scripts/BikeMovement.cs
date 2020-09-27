15111a7d18101966e5f06380634d17089b599e71
Rodolfo <rodolfo.fava@protonmail.com>
2020-09-27 19:16:49 +0200

    [update] adding bike game object, added movement script skeleton.

---
+++ BmxSim/Assets/Scripts/BikeMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BikeMovement : MonoBehaviour
+{
+    Mesh mesh;
+    //Use this for initialization
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        Vector3[] normals = mesh.normals;
+
+        /*if(mesh.CompareTag("FrontWheel"))
+        {
+            
+        }*/
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+       
+
+
+
+    }
+}
